7fabe71cd6cb45caa697fd3c7f3936c6f139c917
bogdan <bkozlovsky@bk.ru>
2020-09-19 19:04:00 +0300

    add cached rate for book and cached likes for comments

--- managebook/models.py
+++ managebook/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Avg
 
 
 class Genre(models.Model):
@@ -29,7 +30,8 @@ class Book(models.Model):
     author = models.ManyToManyField(User, verbose_name="автор", db_index=True)
     publish_date = models.DateField(auto_now_add=True)
     genre = models.ManyToManyField("managebook.Genre", verbose_name="жанр")
-    rate = models.ManyToManyField(User, through="managebook.BookLike", related_name="rate")
+    rate = models.ManyToManyField(User, through="managebook.BookRate", related_name="user")
+    cached_rate = models.DecimalField(max_digits=3, decimal_places=2, default=0.0)
 
     def __str__(self):
         return self.title
@@ -38,12 +40,31 @@ class Book(models.Model):
 class Comment(models.Model):
     text = models.TextField(verbose_name="текст")
     date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="пользователь")
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name="Книга")
-    like = models.ManyToManyField(User, related_name="like")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="пользователь", related_name="comment")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, verbose_name="Книга", related_name="comment")
+    like = models.ManyToManyField(User, through='managebook.CommentLike', related_name="liked_comment")
+    cached_likes = models.PositiveIntegerField(default=0)
 
 
-class BookLike(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+class BookRate(models.Model):
+    class Meta:
+        unique_together = ("user", 'book')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="book_like")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="book_like")
     rate = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.book.cached_rate = self.book.book_like.aggregate(avg_rate=Avg("rate"))['avg_rate']
+        self.book.save()
+
+
+class CommentLike(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.comment.cached_likes += 1
+        self.comment.save()
--- managebook/tests.py
+++ managebook/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from managebook.models import Book, BookRate, Comment, CommentLike
+from django.contrib.auth.models import User
+from django.db.models import Avg
 
+
+class TestRateBook(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create(username="Test1")
+        self.user2 = User.objects.create(username='Test2')
+        self.user3 = User.objects.create(username="Test3")
+
+    def test_rate(self):
+        book = Book.objects.create(title="test title", text="test text", slug="test slug")
+        BookRate.objects.create(user=self.user1, book=book, rate=3)
+        BookRate.objects.create(user=self.user2, book=book, rate=4)
+        BookRate.objects.create(user=self.user3, book=book, rate=2)
+        rate = book.book_like.aggregate(avg_rate=Avg("rate"))
+        self.assertEqual(rate['avg_rate'], 3)
+
+    def test_cached_rate(self):
+        book = Book.objects.create(title="test title", text="test text", slug="test slug")
+        BookRate.objects.create(user=self.user1, book=book, rate=3)
+        BookRate.objects.create(user=self.user2, book=book, rate=4)
+        BookRate.objects.create(user=self.user3, book=book, rate=2)
+        self.assertEqual(book.cached_rate, 3)
+
+    def test_like(self):
+        book = Book.objects.create(title="test title", text="test text", slug="test slug")
+        comment = Comment.objects.create(text="test text", book=book, user=self.user1)
+        comment.like.add(self.user2)
+        comment.like.add(self.user3)
+        likes = comment.like.all().count()
+        self.assertEqual(likes, 2)
+
+    def test_cached_likes(self):
+        book = Book.objects.create(title="test title", text="test text", slug="test slug")
+        comment = Comment.objects.create(text="test text", book=book, user=self.user1)
+        CommentLike.objects.create(comment=comment, user=self.user1)
+        CommentLike.objects.create(comment=comment, user=self.user2)
+        self.assertEqual(comment.cached_likes, 2)
 # Create your tests here.
