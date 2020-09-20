2919e36a4ef3f81db7588a9d6adb42ae8ec4d1f1
bogdan <bkozlovsky@bk.ru>
2020-09-20 21:48:25 +0300

    need solve checker.py

--- managebook/management/commands/checker.py
+++ managebook/management/commands/checker.py
@@ -1,9 +1,18 @@
 from django.core.management import BaseCommand
-from managebook.models import Book, BookRate
-from django.db.models import Case, Count, When, Avg, PositiveIntegerField, Value, Q, F
+from managebook.models import Book
+from django.db.models import Case, Count, When, Avg, PositiveIntegerField, Value, Q, F, CharField
+from django.db.models.functions import Cast
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user_id = 2
-        print(user_id)
+        user_id = 3
+        q = Q(book_like__user_id=user_id)
+        sub_query = Book.objects.filter(~q).annotate(user_rate=Value(0, CharField()))
+        query = Book.objects.filter(q) \
+            .annotate(user_rate=Cast("book_like__rate", CharField())) \
+            .union(sub_query).all()
+        if query:
+            print(vars(query[0]))
+        else:
+            print(query)
