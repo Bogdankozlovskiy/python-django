2cc6dffc5dea623c03271c0df3da4292c993100e
Bogdan <bkozlovsky@bk.ru>
2020-09-21 12:42:22 +0300

    fix views.py

--- managebook/views.py
+++ managebook/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from managebook.models import Book, BookRate, CommentLike
 from django.views.generic import View
-from django.db.models import F, CharField, Value, Q
+from django.db.models import F, CharField, Value, Q, Case, When
 from django.db.models.functions import Cast
 
 
@@ -9,7 +9,8 @@ class HelloView(View):
     def get(self, request):
         if request.user.id:
             q = Q(book_like__user_id=request.user.id)
-            sub_query = Book.objects.filter(~q).annotate(user_rate=Value(0, CharField())) \
+            sub_query = Book.objects.filter(~q) \
+                .annotate(user_rate=Value(0, CharField())) \
                 .prefetch_related('genre', 'author', 'comment__user')
             query = Book.objects.filter(q) \
                 .annotate(user_rate=Cast("book_like__rate", CharField())) \
@@ -22,11 +23,13 @@ class HelloView(View):
 
 class AddComment(View):
     def get(self, request, id):
-        CommentLike.objects.create(user_id=request.user.id, comment_id=id)
+        if request.user.id:
+            CommentLike.objects.create(user_id=request.user.id, comment_id=id)
         return redirect("hello")
 
 
 class AddRate(View):
     def get(self, request, id, rate):
-        BookRate.objects.create(user_id=request.user.id, book_id=id, rate=rate)
+        if request.user.id:
+            BookRate.objects.create(user_id=request.user.id, book_id=id, rate=rate)
         return redirect("hello")
