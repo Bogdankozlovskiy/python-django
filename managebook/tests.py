8ed669a5a40dd9f046a9d2ba2abdd1ab130115ab
bogdan <bkozlovsky@bk.ru>
2020-10-04 17:48:40 +0300

    add a new test

--- managebook/tests.py
+++ managebook/tests.py
@@ -1,7 +1,10 @@
+from time import sleep
 from django.test import TestCase
+from django.urls import reverse
 from managebook.models import Book, BookRate, Comment, CommentLike
 from django.contrib.auth.models import User
 from django.db.models import Avg
+from selenium.webdriver import Chrome
 
 
 class TestRateBook(TestCase):
@@ -12,9 +15,11 @@ class TestRateBook(TestCase):
 
     def test_rate(self):
         book = Book.objects.create(title="test title", text="test text", slug="test slug")
-        BookRate.objects.create(user=self.user1, book=book, rate=3)
-        BookRate.objects.create(user=self.user2, book=book, rate=4)
-        BookRate.objects.create(user=self.user3, book=book, rate=2)
+        BookRate.objects.bulk_create([
+            BookRate(user=self.user1, book=book, rate=3),
+            BookRate(user=self.user2, book=book, rate=4),
+            BookRate(user=self.user3, book=book, rate=2)
+        ])
         rate = book.book_like.aggregate(avg_rate=Avg("rate"))
         self.assertEqual(rate['avg_rate'], 3)
 
@@ -28,8 +33,10 @@ class TestRateBook(TestCase):
     def test_like(self):
         book = Book.objects.create(title="test title", text="test text", slug="test slug")
         comment = Comment.objects.create(text="test text", book=book, user=self.user1)
-        comment.like.add(self.user2)
-        comment.like.add(self.user3)
+        CommentLike.objects.bulk_create([
+            CommentLike(user=self.user1, comment=comment),
+            CommentLike(user=self.user2, comment=comment),
+        ])
         likes = comment.like.all().count()
         self.assertEqual(likes, 2)
 
@@ -39,4 +46,12 @@ class TestRateBook(TestCase):
         CommentLike.objects.create(comment=comment, user=self.user1)
         CommentLike.objects.create(comment=comment, user=self.user2)
         self.assertEqual(comment.cached_likes, 2)
+
+
+class TestInterface(TestCase):
+    def test_ajax_1(self):
+        url = reverse("hello")
+        driver = Chrome()
+        driver.get(f"http://127.0.0.1:8000{url}")
+        sleep(10)
 # Create your tests here.
